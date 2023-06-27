92a7db1d4355c3bad27d9fee377749a4f202a3ba
Michael Koza <michael.koza@pass.de>
2023-06-27 10:29:32 +0200

    Die update Funktionen wurden upgedatet und Fullscreen wurde hinzugefügt

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -12,8 +12,9 @@ class AlienInvasion:
         self.settings = Settings()
 
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
-    
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
@@ -33,22 +34,25 @@ class AlienInvasion:
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                
-                elif event.type == pygame.KEYDOWN:
-                    _check_keydown_events(event)
-                elif event.type == pygame.KEYUP:
-                    _check_keyup_events(event)
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
     
     
     def _check_keydown_events(self,event):
         """Respont to keypresses. """
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
+            print("RIGHT")
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.type == pygame.K_q:
+            print("LEFT")
+
+        elif event.key == pygame.K_q:
+            print("q")
             sys.exit()    
             
     def _check_keyup_events(self,event):
--- AlienInvasion/ship.py
+++ AlienInvasion/ship.py
@@ -31,9 +31,10 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-            
         # Aktualisert das rect-Object auf der Grundlage von self.x.
         self.rect.x = self.x
+        
+        
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image,self.rect)
